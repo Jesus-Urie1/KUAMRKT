86b984d60657f1a4e898ec04d6595285aa30036f
Jesus_Uriel <45404908+Jesus-Urie1@users.noreply.github.com>
2020-10-29 13:23:26 -0600

    Add files via upload

---
+++ index_navbar_lista.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>KUAMRKT</title>
+    <link rel="stylesheet" href="https://bootswatch.com/4/minty/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/index.css">
+    
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <a href="#" class="navbar-brand">KUAMRKT</a>  
+</nav>
+<div class="container-fluid">
+        <div class="row">
+            <div class="col-md-3">
+                    <button type="button" class="btn btn-primary my-sm-3 btn-block">Categoria</button>
+                    <button type="button" class="btn btn-primary my-sm-3 btn-block">Categoria</button>
+                    <button type="button" class="btn btn-primary my-sm-3 btn-block">Categoria</button>
+                    <button type="button" class="btn btn-primary my-sm-3 btn-block">Categoria</button>
+                    <button type="button" class="btn btn-primary my-sm-3 btn-block">Categoria</button>
+                    <button type="button" class="btn btn-primary my-sm-3 btn-block">Categoria</button>
+                    <button type="button" class="btn btn-primary my-sm-3 btn-block">Categoria</button>
+                    <button type="button" class="btn btn-primary my-sm-3 btn-block">Categoria</button>
+                    <button type="button" class="btn btn-primary my-sm-3 btn-block">Categoria</button>
+                    <button type="button" class="btn btn-primary my-sm-3 btn-block">Categoria</button>
+                    <button type="button" class="btn btn-primary my-sm-1 btn-block">Categoria</button>
+                    
+            </div>
+            <div class="col-md-8 bg-primary  my-sm-3 mx-sm-5">
+                <div class="d-flex justify-content-center "><h1>Productos destacados</h1></div>
+                <div></div>
+            </div>
+        </div>
+        
+
+    </div>     
+        
+</body>
+</html>
