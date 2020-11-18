a668d4776fa253abb3855bfc283e58aeda38a423
Jesus_Uriel <45404908+Jesus-Urie1@users.noreply.github.com>
2020-11-17 22:55:50 -0600

    Add files via upload

---
+++ servicios/searchP/searchp_gym.php
@@ -0,0 +1,35 @@
+<?php
+
+include '../../categorias/catbdd/conex.php';
+
+
+$response = new stdClass();
+$datos = array();
+$i=0;
+
+
+$search = $_POST['search'];
+if(!empty($search)){
+    //consulta de busqueda
+    $sql = "SELECT * FROM gym WHERE nombpro LIKE '$search%'";
+    $result = mysqli_query($conex,$sql);
+
+while($row = mysqli_fetch_array($result)){
+    $obj = new stdClass();
+    $obj->nombpro = $row['nombpro'];
+    $obj->precio = $row['precio'];
+    $obj->rutimg = $row['rutimg'];
+
+    $datos[$i]= $obj;
+    $i++;
+}
+
+$response->datos = $datos;
+
+
+mysqli_close($conex);
+header('Content-Type: application/json');
+echo json_encode($response);   
+
+}
+?>
